3ec0662135366465f383bca1af29a30a41e7f7a6
Elenka <cluponka@gmail.com>
2020-03-14 21:18:46 +0200

    gitignore

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 'use strict';
 
-let money = prompt('Ваш бюджет на месяц?');
-let time = prompt('Введите дату в формате YYYY-MM-DD');
+let money = +prompt('Ваш бюджет на месяц?', 0);
+let time = new Date(prompt('Введите дату в формате YYYY-MM-DD', '2020-02-02'));
 
 let appData = {
   finance: money,
   timeData: time,
   expenses: {
-    firstQ: prompt('Введите обязательную статью расходов в этом месяце'),
-    secondQ: prompt('Во сколько обойдется?')
+    firstQ: prompt('Введите обязательную статью расходов в этом месяце', 'комуналка'),
+    secondQ: +prompt('Во сколько обойдется?', 300)
   }
 };
 
-alert('Ваш бютжет на день' + (+appData.finance - +appData.expenses.firstQ) / 30);
+alert('Ваш бютжет на день' + (appData.finance - appData.expenses.firstQ) / 30);
